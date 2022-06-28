ac4d71241e007f7dd8f86e5f599b571a77ad9edc
Daniel-Andarge <andargedaniel90@gmail.com>
2022-06-28 11:23:43 -0700

    Prototype

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,31 +1,25 @@
-#include "lists.h"
-#include <stdio.h>
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
 
 /**
- * check_cycle - checks if singly linked list is a cycle
- * Return: 0 if no cycle, 1 is yes
-*/
-int check_cycle(listint_t *list)
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton project
+ */
+typedef struct listint_s
 {
-	listint_t *fast = list;
-	listint_t *slow = list;
+	int n;
+	struct listint_s *next;
+} listint_t;
 
-	if (!list)
-		return (0);
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+void free_listint(listint_t *head);
+int check_cycle(listint_t *list);
 
-	while (1)
-	{
-		/*traverse through nodes as long as linked list node exists*/
-		if (fast->next != NULL && fast->next->next != NULL)
-		{
-			fast = fast->next->next;
-			slow = slow->next;
-
-			if (fast == slow) /*if nodes match, cycle found*/
-				return (1);
-		}
-		else
-			return (0);
-	}
-
-}
+#endif /* LISTS_H */
