f7031a4c0d6ef3bd952ce770041fa03534b4955b
Daniel-Andarge <andargedaniel90@gmail.com>
2022-10-04 03:57:51 -0700

    Task-3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 
-// Checks for Rectangle parameters
+// Prints a Rectangle with the parameters passed
 
 module.exports = class Rectangle {
-  constructor (w,h) {
+  constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
