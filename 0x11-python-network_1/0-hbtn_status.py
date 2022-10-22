cb7bdde93e047452b2afeabf27a75f5b7136e43f
Daniel-Andarge <andargedaniel90@gmail.com>
2022-10-22 12:08:30 -0700

    fix1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
+"""
+    A script that fetches https://intranet.htbn.io/status
 """
 
-from urllib.request import Request, urlopen
+
+import urllib.request
 
 
 if __name__ == "__main__":
-    req = Request('https://intranet.hbtn.io/status')
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as resp:
+        html = resp.read()
 
-    with urlopen(req) as res:
-        content = res.read()
-        utf8_content = content.decode('utf-8')
-
-        print('Body response:')
-        print('\t- type: {_type}'.format(_type=type(content)))
-        print('\t- content: {_content}'.format(_content=content))
-        print('\t- utf8 content: {_utf8_c}'.format(_utf8_c=utf8_content))
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf8')))
