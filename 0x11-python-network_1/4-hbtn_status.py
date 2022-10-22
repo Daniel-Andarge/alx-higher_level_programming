89b32eeeb1c72a072ae7a3b27d35e9b958c25f7e
Daniel-Andarge <andargedaniel90@gmail.com>
2022-10-22 11:29:18 -0700

    fixed

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """
-    Fetches https://intranet.htbtn.io/status
+fetches https://intranet.hbtn.io/status
 """
-
-import requests
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
+    import requests
     r = requests.get('https://intranet.hbtn.io/status')
+    text = r.text
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
